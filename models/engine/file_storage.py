b0c47cd0202da603b2d18aa0431578c89fe65bc6
arzumirzeb <arzumirzeb03@gmail.com>
2024-04-16 00:03:35 +0400

    improve engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,3 +70,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """close function (opsionel)"""
+        self.reload()
